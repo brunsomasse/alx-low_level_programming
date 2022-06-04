d584450ee6df37696d0b46e8c20270fa2fbc76ba
brunsomasse <brunsomasse@gmail.com>
2022-06-04 07:15:17 -0700

    Task3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - prints the alphabet in lowercase, followed by a new line
+ * 
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int char
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
