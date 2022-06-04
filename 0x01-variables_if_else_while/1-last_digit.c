9c7a3455e96ebdc4a7e323c7db436b3deb436806
brunsomasse <brunsomasse@gmail.com>
2022-06-04 06:03:59 -0700

    Task2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 /**
- *main ->  assign a random number to the variable n each time it is executed
+ *main - >  assign a random number to the variable n each time it is executed
  *print the last digit of the number stored in the variable n
  *return : always 0
  */
 
 int main(void)
-  
+
 {
 int n;
 srand(time(0));
