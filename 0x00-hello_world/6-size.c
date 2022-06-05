df952eb1837f7a4ee8f1d8b19790f63b47ce3f0d
brunsomasse <brunsomasse@gmail.com>
2022-06-05 13:53:51 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,16 +11,16 @@ long long int c;
 char d;
 float e;
 {
-printf("size of an int: %lu byte(s)\n", sizeof(a)); 
+printf("size of a char: %lu byte(s)\n", sizeof(d)); 
+}
+{
+printf("size of a long int: %lu byte(s)\n", sizeof(a));
 }
 {
 printf("size of a long int: %lu byte(s)\n", sizeof(b));
 }
 {
-  printf("size of a long long int: %lu byte(s)\n", sizeof(c));
-}
-{
-printf("size of a char: %lu bytes(s)\n", sizeof(d));
+printf("size of a long long int: %lu bytes(s)\n", sizeof(c));
 }
 {
 printf("size of a float: %lu byte(s)\n", sizeof(e));
