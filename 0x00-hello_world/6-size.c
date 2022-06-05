28e49714d47aa61e3c20d80a9329ccc39f2909df
brunsomasse <brunsomasse@gmail.com>
2022-06-05 13:48:46 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,19 +11,19 @@ long long int c;
 char d;
 float e;
 {
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a)); 
+printf("size of an int: %lu byte(s)\n", sizeof(a)); 
 }
 {
-printf("size of a long int: %lu byte(s)\n", (unsinned long)sizeof(b));
+printf("size of a long int: %lu byte(s)\n", sizeof(b));
 }
 {
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+  printf("size of a long long int: %lu byte(s)\n", sizeof(c));
 }
 {
-printf("size of a char: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("size of a char: %lu bytes(s)\n", sizeof(d));
 }
 {
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("size of a float: %lu byte(s)\n", sizeof(e));
 }
 return (0);
 }
