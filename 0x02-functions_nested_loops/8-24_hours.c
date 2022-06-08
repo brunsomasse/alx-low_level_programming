d6d350f9fc477ca6cb679c25d02c75e321a1e4fe
brunsomasse <brunsomasse@gmail.com>
2022-06-08 10:01:26 -0700

    24Hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  a function that prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+int a; b;
+for (a = 0; a < 24; a++)
+{
+for (b = 0; b < 60; b++)
+{
+if (a < 10)
+{
+_putchar('0');
+_putchar(a + '0');
+}
+else if (a >= 10)
+{
+_putchar((a / 10) + '0');
+_putchar((a % 10) + '0');	
+}
+if (b < 10)
+{
+_putchar(':');
+_putchar('0');
+_putchar(b + '0');
+}
+}
+else if (b > 0)
+{
+_putchar(':');
+_putchar((b / 10) + '0');
+_putchar(b);
+}
+}
+_putchar('\n');
+}
