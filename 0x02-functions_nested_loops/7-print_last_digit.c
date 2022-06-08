32e4119e3919862e04303aa817a941e953800688
brunsomasse <brunsomasse@gmail.com>
2022-06-08 06:10:41 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit -  a function that prints the last digit of a number
+ * @m: character argument
+ * Return: the last digit
+ */
+int print_last_digit(int m)
+{
+int x;
+if (m < 0)
+m = -m;
+x = m % '0';
+_putchar(x + '0');
+return(x);
+}
