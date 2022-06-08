dd90420d221eb0beab5670d9285ad3a96992a9a5
brunsomasse <brunsomasse@gmail.com>
2022-06-08 10:23:03 -0700

    24Hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,8 +17,7 @@ _putchar(a + '0');
 else if (a >= 10)
 {
 _putchar((a / 10) + '0');
-_putchar((a % 10) + '0');	
-}
+_putchar((a % 10) + '0');
 if (b < 10)
 {
 _putchar(':');
@@ -29,7 +28,7 @@ else if (b > 0)
 {
 _putchar(':');
 _putchar((b / 10) + '0');
-_putchar(b);
+_putchar((b % 10) + '0');
 }
 }
 _putchar('\n');
