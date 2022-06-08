3308449c09507d73f46bb4f041b7b08a4a4d67f3
brunsomasse <brunsomasse@gmail.com>
2022-06-07 17:32:27 -0700

    Alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet, in lowercase
+ */
+void print_alphabet_x10(void)
+{
+int m;
+int n;
+for (n = 0; n <= 0; n++)
+{
+for (m = 'a'; m <= 'z'; m++)
+{
+_putchar(m);
+}
+_putchar('\n');
+}
+
+
+
+
+
+
+
+}
