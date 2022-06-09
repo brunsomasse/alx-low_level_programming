f8d1ce57e2246a7767efff60b1fd862a1ac79072
brunsomasse <brunsomasse@gmail.com>
2022-06-09 05:33:29 -0700

    Task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>                                                                                                                     
+#include <stdlib.h>
 #include <time>
 /**
  *main - causes an infinite loop
