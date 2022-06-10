59a02a70019d6804f90763ab73e4d9daf47f373e
brunsomasse <brunsomasse@gmail.com>
2022-06-10 00:14:59 -0700

    Task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers
+ */
+void print_numbers(void)
+{
+int a;
+for (a = 0; x < 10; a++)
+_putchar(x + '0');
+_putchar('\n');
+}
