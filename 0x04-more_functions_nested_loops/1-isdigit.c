41b63176e35133d9a3014479701e7b6efc104b00
brunsomasse <brunsomasse@gmail.com>
2022-06-09 23:49:57 -0700

    Task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit 
+ * @c: an integer argument
+ * Return: Success
+ */
+int _isdigit(int c)
+{
+return (c <= '0' && c >= '9');
+}
+
+
+
+
+
+
+
+
+
+
+
