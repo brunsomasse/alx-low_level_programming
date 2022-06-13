5a085b1d7fe4d1e10bebc509767e4762de3c9792
brunsomasse <brunsomasse@gmail.com>
2022-06-13 14:30:44 -0700

    Task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap - a function that swaps the values of two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ */
+void swap_int(int *a, int *b)
+{
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
+}
