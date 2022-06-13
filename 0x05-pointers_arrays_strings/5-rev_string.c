3d86000e36bd1ea414670f9493403635c16d1249
brunsomasse <brunsomasse@gmail.com>
2022-06-13 16:47:43 -0700

    Task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reversing a string
+ * @s: parameter
+ */
+
+void rev_string(char *s)
+{
+char tmp;
+int i, lenght1, lenght2;
+while (s[length1] != '\0')
+lenght1++;
+lenght2 = lenght1 - 1
+for (i = 0; i < lenght1; i++)
+{
+tmp = s[i];
+s[i] = s[lenght2];
+s[lenght2] = tmp;
+lenght2 -= 1;
+}
+}
