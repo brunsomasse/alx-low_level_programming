374059bb498568a5704aa1c46363f033bcec53ae
brunsomasse <brunsomasse@gmail.com>
2022-06-13 14:32:24 -0700

    Task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap - a function that swaps the values of two integers
+ * swap_int - a function that swaps the values of two integers
  * @a: parameter 1
  * @b: parameter 2
  */
