9b8d760e357e736c8a222eb0e8f476c9c761342d
brunsomasse <brunsomasse@gmail.com>
2022-06-13 17:08:05 -0700

    Task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,10 +9,10 @@ void rev_string(char *s)
 {
 char tmp;
 int i, lenght1, lenght2;
-while ([length1] != '\0')
+while (s[lenght1] != '\0')
 lenght1++;
 lenght2 = lenght1 - 1;
-for (i = 0; i < lenght1 / 2; i++)
+for (i = 0; i < length1 / 2; i++)
 {
 tmp = s[i];
 s[i] = s[lenght2];
