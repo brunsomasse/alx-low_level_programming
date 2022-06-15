3ed6db44bee5f7402482b37e43e1ad462ef7e86f
brunsomasse <brunsomasse@gmail.com>
2022-06-14 22:34:11 -0700

    Task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,6 +9,8 @@ void rev_string(char *s)
 {
 char tmp;
 int i, length1, length2;
+length1 = 0;
+length2 = 0;
 while (s[length1] != '\0')
 length1++;
 length2 = length1 - 1;
@@ -17,6 +19,6 @@ for (i = 0; i < length1; i++)
 tmp = s[i];
 s[i] = s[length2];
 s[length2] = tmp;
-length2 -= 1;
+length2 -= 2;
 }
 }
