ad21b7af4e9b361e7f006a71f77c4bc8d0b378d2
brunsomasse <brunsomasse@gmail.com>
2022-06-16 13:44:24 -0700

    Task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *string_toupper - a function that changes all lowercase letters of a string
+ * string_toupper - a function that changes all lowercase letters of a string
  * @x: string parameter
  * Return: string
  */
