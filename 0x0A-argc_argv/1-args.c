d0374cd248000067995642c48957b4063d7ab7a1
brunsomasse <brunsomasse@gmail.com>
2022-06-26 09:38:23 -0700

    Task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -  a program that prints the number of arguments
+ * @argc: argc parameter
+ * @argv: arrays of command listed
+ * Return: 0 for success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
