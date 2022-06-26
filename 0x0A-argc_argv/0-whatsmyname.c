e87af52eba62c4f6f0298613586ee72b28ee731c
brunsomasse <brunsomasse@gmail.com>
2022-06-26 08:39:32 -0700

    Task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,12 @@
 
 /**
  * main - a program that prints its name, followed by a new line
+ * @argc: argc parameter
  * @argv: array of command listed
  * Return: Always success
  */
 
-int main(char *argv[])
+int main(int argc _attribute_((unused)), char *argv[])
 {
 printf("%s\n", *argv);
 return (0);
